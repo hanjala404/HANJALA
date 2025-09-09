a10a152756bfeadd8aecb216cbd9655927bdff56
hanjala404 <hanjalabottem@gmail.com>
2025-09-09 14:31:50 +0500

    Update outall.js

--- Script/commands/outall.js
+++ Script/commands/outall.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "outall",
 	version: "1.0.0",
-	hasPermssion: 2,
+	hasPermssion: 4,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Send messages to groups!",
 	commandCategory: "Admin",
